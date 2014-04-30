0d1d556372178a06fa41171d278a76d6347bf45b
Yihui Xie <xie@yihui.name>
2014-04-30 13:46:03 -0500

    some blank lines

--- R/mime.R
+++ R/mime.R
@@ -83,16 +83,19 @@ mimeextra = c(
 #' # not in the standard table, but in mimeextra
 #' guess_type(c('a.md', 'b.R'), mime_extra = NULL)
 #' guess_type(c('a.md', 'b.R'))
+#'
 #' # override the standard MIME table (tex is text/x-tex by default)
 #' guess_type('tex', mime_extra = c(tex = 'text/plain'))
 #' # unknown extension 'bar'
 #' guess_type('foo.bar')
 #' # force unknown types to be plain text
 #' guess_type('foo.bar', unknown = 'text/plain')
+#'
 #' # empty file extension
 #' guess_type('Makefile')
 #' # we know it is a plain text file
 #' guess_type('Makefile', empty = 'text/plain')
+#'
 #' # subtypes
 #' guess_type(c('abc.html', 'def.htm'), subtype = c('charset=UTF-8', ''))
 #' @export
