642084da2ed0c83ba65bf197f0541c9c8b8fa887
Yihui Xie <xie@yihui.name>
2018-10-05 11:31:21 -0500

    regenerate the mime map under Ubuntu 18.04

--- R/mime.R
+++ R/mime.R
@@ -59,8 +59,6 @@ local({
 #' @format NULL
 mimeextra = c(
   geojson = "application/vnd.geo+json",
-  md = "text/x-markdown",
-  markdown = "text/x-markdown",
   r = "text/plain",
   rd = "text/plain",
   rmd = "text/x-markdown",
