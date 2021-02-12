803543bd138cdd22b03977d67458d81e00824675
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2021-02-12 22:39:12 +0100

    Add mimetype for OGC GeoPackage (.gpkg extension) (#14)

--- R/mime.R
+++ R/mime.R
@@ -59,6 +59,7 @@ local({
 #' @format NULL
 mimeextra = c(
   geojson = "application/vnd.geo+json",
+  gpkg = "application/geopackage+sqlite3",
   jsonp = "application/javascript",
   r = "text/plain",
   rd = "text/plain",
